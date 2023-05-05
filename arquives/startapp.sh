d6120516746fbf6a9d442e54f62f0ee0b84e623d
Alan Bruno <alanbruno.rj@gmail.com>
2023-05-05 17:43:33 -0300

    Added authentication through active directory

--- arquives/startapp.sh
+++ arquives/startapp.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Run the broadwayd daemon and point nginx to it
 /usr/local/bin/start
-sed -i 's/$DOMINIO/'$DOMINIO'/' /etc/krb5.conf
-sed -i 's/$HOST_DOMINIO_1/'$HOST_DOMINIO_1'/' /etc/krb5.conf
-sed -i 's/%CORNER_IMAGE_URL%/'$CORNER_IMAGE_URL'/' /etc/nginx/sites-available/default
-sed -i 's/%APP_TITLE%/'$APP_TITLE'/' /etc/nginx/sites-available/default
-sed -i 's/%CORNER_IMAGE_URL%/'$CORNER_IMAGE_URL'/' /etc/nginx/nginx.tmpl
-sed -i 's/%APP_TITLE%/'$APP_TITLE'/' /etc/nginx/nginx.tmpl
+#sed -i 's/$DOMINIO/'$DOMINIO'/' /etc/krb5.conf
+#sed -i 's/$HOST_DOMINIO_1/'$HOST_DOMINIO_1'/' /etc/krb5.conf
+
 # Note: No X server or wayland support--only cli and gtk3
 # ↓↓↓ PUT COMMANDS HERE ↓↓↓
 dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS"
