632535367ff544a256c9547d7a2e4e43452642d8
Alan Bruno <33059939+alan-bruno@users.noreply.github.com>
2023-07-14 21:30:57 -0300

    Update startapp.sh

--- files/startapp.sh
+++ files/startapp.sh
@@ -6,9 +6,9 @@ sed -i 's/$HOST_DOMINIO_1/'$HOST_DOMINIO_1'/' /etc/krb5.conf
 sed -i 's/$HOST_DOMINIO_2/'$HOST_DOMINIO_2'/' /etc/krb5.conf
 # Note: No X server or wayland support--only cli and gtk3
 # ↓↓↓ PUT COMMANDS HERE ↓↓↓
-dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS"
-dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$HOSTS"
-dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
+#dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$HOSTS"
+#dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$HOSTS"
+#dbus-launch gsettings set org.virt-manager.virt-manager xmleditor-enabled true
 while true; 
 do 
 ps -C virt-manager > /dev/null
